7ac4794456a018b0da4b1ac6d05d3d06a263bb22
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2022-12-09 10:26:07 +0100

    Added AirQualitySystem_bp

---
+++ src/nodes/boilerplate/AirQualitySystem_bp/AirQualitySystem_bp.ino
@@ -0,0 +1,23 @@
+/**
+ * @file AirQualitySystem_bp.ino
+ *
+ * @author Jonathan Camenzuli
+ *
+ * @brief Boilerplate for Air Quality System
+ *
+ * Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
+ * "Miniature implementation of an IoT-based Smart City"
+ *
+ * @date DD/MM/YYY
+ *
+ * @section libraries Libraries
+ * - Low-Power by @rocketscream (https://github.com/rocketscream/Low-Power)
+ * - General Utility Header File (../../fyp_utils/fyp_utils.h)
+ *
+ * @section hardware Hardware
+ * -  Arduino Nano
+ * -  MQ135 Hazardous Gas Detection Module
+ */
+
+#include "LowPower.h"
+#include "fyp_utils.h"
