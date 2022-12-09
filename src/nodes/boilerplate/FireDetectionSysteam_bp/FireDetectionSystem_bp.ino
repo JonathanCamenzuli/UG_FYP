99057945cf6da54ce6317b0cc7ec8e9a798f86c7
GanniMT <GanniMT@users.noreply.github.com>
2022-12-09 10:27:50 +0100

    Added FireDetectionSystem_bp

---
+++ src/nodes/boilerplate/FireDetectionSysteam_bp/FireDetectionSystem_bp.ino
@@ -0,0 +1,23 @@
+/**
+ * @file FireDetectionSystem_bp.ino
+ *
+ * @author Jonathan Camenzuli
+ *
+ * @brief Boilerplate for Fire Detection System
+ *
+ * Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
+ * "Miniature implementation of an IoT-based Smart City"
+ *
+ * @date DD/MM/YYY
+ *
+ * @section libraries Libraries
+ * - Low-Power by @rocketscream (https://github.com/rocketscream/Low-Power)
+ * - General Utility Header File (../../fyp_utils/fyp_utils.h)
+ *
+ * @section hardware Hardware
+ * -  Arduino Nano
+ * -  Flame Sensor Module
+ */
+
+#include "LowPower.h"
+#include "fyp_utils.h"
