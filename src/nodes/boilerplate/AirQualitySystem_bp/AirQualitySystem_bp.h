9f4b19307ac7a9f0336467956e01e17b99c9a522
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2023-02-11 01:09:40 +0100

    Created AirQualitySystem_bp header file

---
+++ src/nodes/boilerplate/AirQualitySystem_bp/AirQualitySystem_bp.h
@@ -0,0 +1,59 @@
+/**
+ * @file AirQualitySystem_bp.h
+ *
+ * @author Jonathan Camenzuli
+ *
+ * @brief Boilerplate for Air Quality Monitoring System Header File
+ *
+ * Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
+ * "Miniature implementation of an IoT-based Smart City"
+ *
+ * @date 11/02/2023
+ *
+ * @section libraries Libraries
+ * - ArduinoJson by @bblanchon (https://github.com/bblanchon/ArduinoJson)
+ */
+
+#ifndef __AQMS_BP_H
+#define __AQMS_BP_H
+
+#include <ArduinoJson.h>
+
+/**
+ * @brief Function returns serialised JSON document for Air Quality Monitoring System
+ *
+ * @param temp      The `temperature_c` variable
+ * @param hum       The `humidity_percent` variable
+ * @param co        The `co_level_ppm` variable
+ * @param co2       The `co2_level_ppm` variable
+ * @return String   Serialised JSON document
+ */
+String serializeJson(float &temp, float &hum, float &co, float &co2)
+{
+    DynamicJsonDocument jsonDoc(64);
+
+    // Set the values of the JSON packet
+    jsonDoc["nodetype"] = "AQMS";
+    jsonDoc["id"] = "aqms0001";
+
+    // Creating and setting the value for the data nested object
+    JsonObject data = jsonDoc.createNestedObject("data");
+    data["temperature_c"] = temp;
+    data["humidity_percent"] = hum;
+    data["co_level_ppm"] = co;
+    data["co2_level_ppm"] = co2;
+
+    // Create a string for storing the serialized JSON document
+    String buffer;
+
+    // Serialize the JSON packet
+    serializeJson(jsonDoc, buffer);
+
+    // Free the memory occupied by the JSON document
+    jsonDoc.clear();
+
+    // Return the serialized JSON document as a string
+    return buffer;
+}
+
+#endif
--- src/nodes/boilerplate/AirQualitySystem_bp/AirQualitySystem_bp.ino
+++ src/nodes/boilerplate/AirQualitySystem_bp/AirQualitySystem_bp.ino
@@ -3,7 +3,7 @@
  *
  * @author Jonathan Camenzuli
  *
- * @brief Boilerplate for Air Quality System
+ * @brief Boilerplate for Air Quality Monitoring System
  *
  * Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
  * "Miniature implementation of an IoT-based Smart City"
@@ -12,8 +12,8 @@
  *
  * @section libraries Libraries
  * - Low-Power by @rocketscream (https://github.com/rocketscream/Low-Power)
- * - ArduinoJson by @bblanchon (https://github.com/bblanchon/ArduinoJson)
  * - General Utility Header File (../../fyp_utils/fyp_utils.h)
+ * - Boilerplate for Air Quality Monitoring System Header File (./AirQualitySystem_bp.h)
  *
  * @section hardware Hardware
  * -  Arduino Nano
@@ -22,32 +22,4 @@
 
 #include "LowPower.h"
 #include "fyp_utils.h"
-#include <ArduinoJson.h>
-
-String serializeJson(float &temp, float &hum, float &co, float &co2)
-{
-    DynamicJsonDocument jsonDoc(64);
-
-    // Set the values of the JSON packet
-    jsonDoc["nodetype"] = "AQMS";
-    jsonDoc["id"] = "aqms0001";
-
-    // Creating and setting the value for the data nested object
-    JsonObject data = jsonDoc.createNestedObject("data");
-    data["temperature_c"] = temp;
-    data["humidity_percent"] = hum;
-    data["co_level_ppm"] = co;
-    data["co2_level_ppm"] = co2;
-
-    // Create a string for storing the serialized JSON document
-    String buffer;
-
-    // Serialize the JSON packet
-    serializeJson(jsonDoc, buffer);
-
-    // Free the memory occupied by the JSON document
-    jsonDoc.clear();
-
-    // Return the serialized JSON document as a string
-    return buffer;
-}
+#include "AirQualitySystem_bp.h"
