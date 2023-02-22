681686c789108278f61b68124320c9967df89cb1
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2023-02-22 16:59:48 +0100

    Revert "Seperated docker-compose run and enforced bridged connections in docker"
    This reverts commit 59f1f6be4c512a58c3828ef467a8cd37fe6f8a4d.

--- src/server/docker-build.ps1
+++ src/server/docker-build.ps1
@@ -2,5 +2,5 @@ docker network create fyp_docker_network
 docker volume create influxdb_volume
 docker volume create grafana_volume
 docker-compose run --detach -p 5683:5683/udp coap-server
-docker-compose run --detach -p 8086:8086 influxdb
-docker-compose run --detach -p 3001:3001 grafana
+docker-compose run --detach -p 8086:8086/tcp influxdb
+docker-compose run --detach -p 3001:3001/tcp grafana
