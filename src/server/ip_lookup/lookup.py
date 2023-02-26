e2e94e54984cf5a84d9cedc92777c0d5063d5446
GanniMT <GanniMT@users.noreply.github.com>
2023-02-27 00:07:59 +0100

    Added Public IP Lookup Script

---
+++ src/server/ip_lookup/lookup.py
@@ -0,0 +1,31 @@
+##
+# @file lookup.py
+#
+# @author Jonathan Camenzuli
+#
+# @brief Public IP Lookup
+#
+# Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
+# "Miniature implementation of an IoT-based Smart City"
+#
+# @date 26/02/2022
+#
+# @section libraries Libraries
+# - requests by Python Software Foundation (https://github.com/psf/requests)
+# - flask by Pallets Projects (https://github.com/pallets/flask)
+#
+
+import requests
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_public_ip():
+    ip_address = requests.get('https://api.ipify.org')
+    return ip_address.text
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
