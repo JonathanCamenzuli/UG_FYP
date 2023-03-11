b8bd3170a90236a89713abf8bfd756207efc3377
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2023-03-11 15:10:00 +0100

    Resolved Restart Policy Issue in Powershell Script

--- src/server/docker-build.ps1
+++ src/server/docker-build.ps1
@@ -1,9 +1,53 @@
+##
+# docker-build.ps1
+#
+# Jonathan Camenzuli
+#
+# Powershell Script Which Creates and Configures Docker Containers 
+#
+# Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
+# "Miniature implementation of an IoT-based Smart City"
+#
+# 11/03/2023
+#
+#
+
+# NOTE: ONLY EXECUTE ONCE!
+
+# Create Networks
 docker network create fyp_backend
 docker network create fyp_frontend
 docker network create fyp_ip_lookup
+
+# Create Volumes
 docker volume create influxdb_volume
 docker volume create grafana_volume
+
+# Start containers and get ID
 docker-compose run --detach -p 5683:5683/udp coap-server
 docker-compose run --detach -p 8086:8086/tcp influxdb
 docker-compose run --detach -p 3001:3001/tcp grafana
 docker-compose run --detach -p 80:80 ip-lookup
+
+$coap_id = docker ps --quiet --filter 'name=^fyp_smart_city_model_coap-server'
+$influxdb_id = docker ps --quiet --filter 'name=^fyp_smart_city_model_influxdb'
+$grafana_id = docker ps --quiet --filter 'name=^fyp_smart_city_model_grafana'
+$iplookup_id = docker ps --quiet --filter 'name=^fyp_smart_city_model_ip-lookup'
+
+# Stop Containers
+docker stop --time 1 $coap_id
+docker stop --time 1 $influxdb_id
+docker stop --time 1 $grafana_id
+docker stop --time 1 $iplookup_id
+
+# Update Restart policy
+docker update --restart always $coap_id
+docker update --restart always $influxdb_id
+docker update --restart always $grafana_id
+docker update --restart always $iplookup_id
+
+# Start Containers Again
+docker start $coap_id
+docker start $influxdb_id
+docker start $grafana_id
+docker start $iplookup_id
