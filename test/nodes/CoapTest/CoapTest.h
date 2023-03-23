ed6490fe71d3cb6132dca8641c9a1fdca0be5c7a
GanniMT <GanniMT@users.noreply.github.com>
2023-03-23 15:02:27 +0100

    Abstraction if serialiseJson successful in CoapTest

--- test/nodes/CoapTest/CoapTest.h
+++ test/nodes/CoapTest/CoapTest.h
@@ -20,85 +20,20 @@
 #define __COAPTEST_H
 
 #include <MKRNB.h>
-#include <ArduinoJson.h>
 #include <coap-simple.h>
-#include "arduino_secrets.h"
 
 #define SLEEP_TIME_MS 40000 // Sleep time, in milliseconds
-#define BUF_SIZE 1000        // Buffer Size
+#define BUF_SIZE 500        // Buffer Size
 
 /**
  * @brief Function returns serialised JSON document for CoAP Test
  *
  * @return String Serialised JSON Document
  */
-char *serialiseJson(float testVal)
-{
-    // Size calculated on https://arduinojson.org/v6/assistant/
-    StaticJsonDocument<96> jsonDoc;
+void serialiseJson(float &testVal, char *buffer);
 
-    // Create a string for storing the serialized JSON document
-    char buffer[BUF_SIZE];
+bool connectNB(NB nbAccess, GPRS gprsAccess);
 
-    // Set the values of the JSON packet
-    jsonDoc["nodetype"] = "TEST";
-    jsonDoc["id"] = "test0001";
-
-    // Creating and setting the value for the data nested object
-    JsonObject data = jsonDoc.createNestedObject("data");
-    data["testValue"] = testVal;
-
-    // Serialize the JSON document
-    serializeJson(jsonDoc, buffer);
-
-    // Free the memory occupied by the JSON document
-    jsonDoc.clear();
-
-    Serial.println(buffer);
-
-    // Return the serialized JSON document as a string
-    return buffer;
-}
-
-bool connectNB(NB nbAccess, GPRS gprsAccess)
-{
-    Serial.print("Connecting to ISP...");
-    // Failed, retry
-    while (nbAccess.begin(SECRET_PINNUMBER, SECRET_GPRS_APN) != NB_READY || gprsAccess.attachGPRS() != GPRS_READY)
-    {
-        Serial.print(".");
-        delay(1000);
-    }
-    Serial.println("done.");
-
-    return true;
-}
-
-uint16_t sendPacket(IPAddress coapServer_ip, Coap coap)
-{
-    // Generate random number
-    float rand = 20 + random(0, 9);
-
-    // Package JSON Document
-    char *jsonDoc = serialiseJson(rand);
-    Serial.println(jsonDoc);
-
-    uint16_t msgid = coap.put(coapServer_ip, SECRET_COAP_PORT, SECRET_COAP_ENDPOINT1, jsonDoc);
-
-    // // Send a CoAP POST message to CoAP server
-    // uint16_t msgid = coap.send(
-    //     coapServer_ip,        // IP address
-    //     SECRET_COAP_PORT,     // Port
-    //     SECRET_COAP_ENDPOINT, // CoAP endpoint
-    //     COAP_CON,             // CoAP packet type (con, noncon, ack, reset)
-    //     COAP_POST,            // CoAP method
-    //     NULL,                 // CoAP token
-    //     0,                    // CoAP token length
-    //     (uint8_t *)buffer,    // Message payload
-    //     buf_size              // Message payload length
-    // );
-
-    return msgid;
-}
+uint16_t sendPacket(IPAddress &coapServer_ip, Coap &coap, float &rand);
 
 #endif
--- test/nodes/CoapTest/CoapTest.ino
+++ test/nodes/CoapTest/CoapTest.ino
@@ -11,15 +11,15 @@
  * @date 15/03/2023
  *
  * @section libraries Libraries
- * - ArduinoLowPower by Arduino Libraries (https://github.com/arduino-libraries/ArduinoLowPower)
+ * - ArduinoJson by Benoît Blanchon (https://github.com/bblanchon/ArduinoJson)
  * - MKRNB by Arduino Libraries (https://github.com/arduino-libraries/MKRNB)
+ * - CoAP-simple-library by Hirotaka Niisato (https://github.com/hirotakaster/CoAP-simple-library)
  * - CoAP Test Header File (./CoapTest.h)
  *
  * @section hardware Hardware
  * -  Arduino MKR NB 1500
  */
 
-#include <ArduinoLowPower.h>
 #include <MKRNB.h>
 #include <ArduinoJson.h>
 #include <coap-simple.h>
@@ -60,7 +60,7 @@ IPAddress coapServer_ip;
 
 void setup()
 {
-
+  // Temporary
   coapServer_ip.fromString((coapIP));
 
   // Open serial communication and wait for port to open
@@ -68,34 +68,26 @@ void setup()
   while (!Serial)
     ;
 
-  /**
-   * Issue a series of AT commands to the modem.
-   *
-   *    - Wait for it to get ready
-   *    - Disconnect from any connected networks
-   *    - Set RAT (NB-IoT or LTE-M)
-   *    - Apply changes and save configuration
-   *    - Turn radio on
-   *    - Check attachment
-   *    - Set operator
-   */
-
+  // Wait for it to get ready
   Serial.print("Waiting for modem to get ready...");
   MODEM.begin();
   while (!MODEM.noop())
     ;
   Serial.println("done.");
 
+  // Disconnect from any connected networks
   Serial.print("Disconnecting from network...");
   MODEM.send("AT+COPS=2");
   MODEM.waitForResponse(2000);
   Serial.println("done.");
 
+  // Set RAT (NB-IoT or LTE-M)
   Serial.print("Setting Radio Access Technology to NB-IoT or LTE-M (7 is for LTE-M and 8 is for NB-IoT)...");
   MODEM.sendf("AT+URAT=%d", rat);
   MODEM.waitForResponse(2000);
   Serial.println("done.");
 
+  // Apply changes and save configuration
   Serial.print("Applying changes and saving configuration...");
   MODEM.send("AT+CFUN=15");
   do
@@ -105,11 +97,13 @@ void setup()
   } while (MODEM.waitForResponse(1000) != 1);
   Serial.println("done.");
 
+  // Turn radio on
   Serial.print("Modem ready, turn radio on in order to configure it...");
   MODEM.send("AT+CFUN=1");
   MODEM.waitForResponse(2000);
   Serial.println("done.");
 
+  // Check attachment
   Serial.print("Check attachment until CSQ RSSI indicator is less than 99...");
   String response;
   int status = 99;
@@ -123,11 +117,6 @@ void setup()
   }
   Serial.println("done.");
 
-  // Serial.print("Set operator to Telenor...");
-  // MODEM.sendf("AT+COPS=1,2,\"%d\"", cops);
-  // MODEM.waitForResponse(4000);
-  // Serial.println("done.");
-
   // Check if connected and if not, reconnect
   if (nbAccess.status() != NB_READY || gprsAccess.status() != GPRS_READY)
   {
@@ -136,62 +125,26 @@ void setup()
 
   // Initialize CoAP client
   coap.start();
-
-  // Seed random number generator with noise from pin 0
-  randomSeed(analogRead(0));
 }
 
 void loop()
 {
-  // Check if connected and if not, reconnect
-  // if (nbAccess.status() != NB_READY || gprsAccess.status() != GPRS_READY)
-  // {
-  //   connectNB(nbAccess, gprsAccess);
-  // }
-
-  // TEMPORARY !!
+  // Seed random number generator with noise from pin 0
+  randomSeed(analogRead(0));
 
   Serial.print("Sending packet to CoAP Server");
 
   // Generate random number
   float rand = 20 + random(0, 9);
 
-  // Package JSON Document
-  // char *jsonDoc = serialiseJson(rand);
+  sendPacket(coapServer_ip, coap, rand);
 
-  //////////////////////////////////////// serialise
-  // Size calculated on https://arduinojson.org/v6/assistant/
-  StaticJsonDocument<96> jsonDoc;
-
-  // Create a string for storing the serialized JSON document
-  char buffer[BUF_SIZE];
-
-  // Set the values of the JSON packet
-  jsonDoc["nodetype"] = "TEST";
-  jsonDoc["id"] = "test0001";
-
-  // Creating and setting the value for the data nested object
-  JsonObject data = jsonDoc.createNestedObject("data");
-  data["testValue"] = rand;
-
-  // Serialize the JSON document
-  serializeJson(jsonDoc, buffer);
-
-  // Free the memory occupied by the JSON document
-  jsonDoc.clear();
-
-  Serial.println(buffer);
-  // Serial.println(jsonDoc);
-
-  // uint16_t msgid = coap.put(coapServer_ip, SECRET_COAP_PORT, SECRET_COAP_ENDPOINT1, jsonDoc);
-  uint16_t msgid = coap.put(coapServer_ip, SECRET_COAP_PORT, SECRET_COAP_ENDPOINT, buffer);
-
-  // sendPacket(coapServer_ip, coap);
   Serial.println("done.");
 
   // Continue CoAP operations (handle ACK, send response)
   coap.loop();
 
-  Serial.println("Wait 10s before sending again...");
-  delay(10000);
+  delay(30000);
+  Serial.println("30s passed...");
+  // Serial.println("Wait 30s before sending again...");
 }
