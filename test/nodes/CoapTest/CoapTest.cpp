77faaf0a7d0b740e065013b3086dd977da3808a4
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2023-03-26 18:27:42 +0200

    Successfully integrated IPLookup functionality into CoapTest

--- test/nodes/CoapTest/CoapTest.cpp
+++ test/nodes/CoapTest/CoapTest.cpp
@@ -4,6 +4,52 @@
 #include <coap-simple.h>
 #include <ArduinoJson.h>
 
+void setupModem()
+{
+    // Wait for it to get ready
+    Serial.print("Waiting for modem to get ready...");
+    MODEM.begin();
+    while (!MODEM.noop())
+        ;
+    Serial.println("done.");
+
+    // Set RAT to NB-IoT
+    Serial.print("Setting Radio Access Technology to NB-IoT...");
+    MODEM.sendf("AT+URAT=%d", 8);
+    MODEM.waitForResponse(2000);
+    Serial.println("done.");
+
+    // Apply changes and save configuration
+    Serial.print("Applying changes and saving configuration...");
+    MODEM.send("AT+CFUN=15");
+    do
+    {
+      delay(1000);
+      MODEM.noop();
+    } while (MODEM.waitForResponse(1000) != 1);
+    Serial.println("done.");
+
+    // Turn radio on
+    Serial.print("Modem ready, turn radio on in order to configure it...");
+    MODEM.send("AT+CFUN=1");
+    MODEM.waitForResponse(2000);
+    Serial.println("done.");
+
+    // Check attachment
+    Serial.print("Check attachment until CSQ RSSI indicator is less than 99...");
+    String response;
+    int status = 99;
+    while (status > 98 && status > 0)
+    {
+      MODEM.send("AT+CSQ");
+      MODEM.waitForResponse(2000);
+      String sub = response.substring(6, 8);
+      status = sub.toInt(); // Will return 0 if no valid number is found
+      delay(1000);
+    }
+    Serial.println("done.");
+}
+
 void serialiseJson(float &testVal, char *buffer)
 {
     // Size calculated on https://arduinojson.org/v6/assistant/
@@ -24,7 +70,7 @@ void serialiseJson(float &testVal, char *buffer)
     jsonDoc.clear();
 }
 
-bool connectNB(NB nbAccess, GPRS gprsAccess)
+bool connectNB(NB &nbAccess, GPRS &gprsAccess)
 {
     Serial.print("Connecting to ISP...");
     // Failed, retry
@@ -41,7 +87,8 @@ bool connectNB(NB nbAccess, GPRS gprsAccess)
 void sendPacket(IPAddress &coapServer_ip, Coap &coap, float &rand)
 {
     // Create a string for storing the serialized JSON document
-    char *jsonDocBuf = (char*)malloc(BUF_SIZE);
+    char *jsonDocBuf = (char *)malloc(BUF_SIZE);
+    // alternative: char jsonDocBuf[BUF_SIZE];
 
     // Package JSON Document
     serialiseJson(rand, jsonDocBuf);
@@ -49,4 +96,25 @@ void sendPacket(IPAddress &coapServer_ip, Coap &coap, float &rand)
 
     coap.put(coapServer_ip, SECRET_COAP_PORT, SECRET_COAP_ENDPOINT, jsonDocBuf);
     free(jsonDocBuf);
+}
+
+void getIPAddress(IPAddress &ipAddress, HttpClient &httpClient)
+{
+    int statusCode;
+    String response;
+
+    httpClient.get("/");
+
+    statusCode = httpClient.responseStatusCode();
+    response = httpClient.responseBody();
+
+    if (statusCode != 200)
+    {
+        Serial.print("Status Code: ");
+        Serial.println(statusCode);
+        Serial.print("Server Not Found");
+        ipAddress.fromString("0.0.0.0");
+    }
+
+    ipAddress.fromString(response);
 }
