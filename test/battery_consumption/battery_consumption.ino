8a43e548f23b32efc45a3b8d612025e824bb07e0
GanniMT <GanniMT@users.noreply.github.com>
2023-05-12 13:00:54 +0200

    Added initial version for Battery Consumption Data Logging - UNTESTED

---
+++ test/battery_consumption/battery_consumption.ino
@@ -0,0 +1,16 @@
+/**
+ * @file battery_consumption.ino
+ *
+ * @author Jonathan Camenzuli
+ *
+ * @brief Battery Consumption Monitor
+ *
+ * Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
+ * "Miniature implementation of an IoT-based Smart City"
+ *
+ * @date 12/05/2023
+ *
+ * @section hardware Hardware
+ * -  Arduino UNO
+ * -  Voltage Sensor
+ */
