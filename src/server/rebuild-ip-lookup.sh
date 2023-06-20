2929a4f909c1fdf615dac514f31c3fe9a1bc5efb
GanniMT <GanniMT@users.noreply.github.com>
2023-06-20 12:27:55 +0200

    Modified IP Lookup Server such that there is a fallback API to use

---
+++ src/server/rebuild-ip-lookup.sh
@@ -0,0 +1,32 @@
+##
+# rebuild-ip-lookup.sh
+#
+# Jonathan Camenzuli
+#
+# Bash Script Which Rebuilds IP Lookup Server
+#
+# Source code is part of my Final Year Project in Computer Engineering (2022/23) entitled
+# "Miniature implementation of an IoT-based Smart City"
+#
+# 20/06/2023
+#
+#
+
+# Get ID of container
+ip_lookup_id=$(docker ps --quiet --filter 'name=^fyp_smart_city_model_ip-lookup')
+
+# Stop Container
+docker stop --time 0 $ip_lookup_id
+
+# Remove Continer
+docker rm $ip_lookup_id
+
+# Remove Image
+docker image rm fyp_smart_city_model_ip-lookup
+
+# docker-build routine for IP Lookup server
+docker compose run --detach -p 80:80/tcp ip-lookup
+new_ip_lookup_id=$(docker ps --quiet --filter 'name=^fyp_smart_city_model_ip-lookup')
+docker stop --time 0 $new_ip_lookup_id
+docker update --restart always $new_ip_lookup_id
+docker start $new_ip_lookup_id
