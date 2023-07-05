f0db2a7430c03c1cb1263e7d632d8a21f56cf53f
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2023-07-05 19:41:56 +0200

    Added python docstrings to IP Lookup server source code

--- src/server/ip_lookup/lookup.py
+++ src/server/ip_lookup/lookup.py
@@ -24,8 +24,18 @@ time.tzset()
 
 app = flask.Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def get_public_ip():
+    """
+    Gets Public IP Address of The Server
+
+    This function retrieves the public IP address of the server. It first tries to retrieve the IP address using the ipify service. If the ipify service is not available, it falls
+    back to using another service. The IP address is returned as a string.
+
+    Returns:
+        str: The public IP address.
+    """
     try:
         ip_address = get_ip()
         return ip_address
@@ -33,6 +43,7 @@ def get_public_ip():
         # Using another service in case ipify is down
         ip_address = get('https://ident.me/').text
         return ip_address
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
